375a9b8c0d4097db3ee85983cba3f484ee4b7049
Aleksi Moisio <aleksi.moisio30@gmail.com>
2019-09-11 22:33:30 +0300

    Removed commented code and a bug fix

--- MathExtensions/PrimesNew.cs
+++ MathExtensions/PrimesNew.cs
@@ -66,7 +66,7 @@ namespace MathExtensions
                 {
                     primes.Add(candidate);
                     yield return candidate;
-                    if (++generated > n) yield break;
+                    if (++generated >= n) yield break;
                 }
             }
         }
