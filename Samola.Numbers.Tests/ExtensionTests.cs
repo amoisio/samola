822c4724157318d7ef13accb74585ff8068e52d9
Aleksi Moisio <aleksi.moisio30@gmail.com>
2019-10-13 22:47:46 +0300

    Add Substrings string extension method + tests.

---
+++ Samola.Numbers.Tests/ExtensionTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Samola.Numbers.Utilities;
+using Xunit;
+
+namespace Samola.Numbers.Tests
+{
+    public class ExtensionTests
+    {
+        [Fact]
+        public void Negative_startIndex_throws_an_error()
+        {
+            try
+            {
+                "hello".Substrings(-1, 2);
+                Assert.False(true);
+            }
+            catch (Exception)
+            {
+                Assert.True(true);
+            }
+        }
+
+        [Fact]
+        public void OutOfBounds_startIndex_throws_an_error()
+        {
+            try
+            {
+                "hello".Substrings(5, 2);
+                Assert.False(true);
+            }
+            catch (Exception)
+            {
+                Assert.True(true);
+            }
+        }
+
+        [Theory]
+        [InlineData("0123456789", 0, 3, new string[] { "012", "345", "678", "9" })]
+        [InlineData("012345678", 0, 3, new string[] { "012", "345", "678" })]
+        [InlineData("012 456 8", 0, 3, new string[] { "012", " 45", "6 8" })]
+        public void Substrings_splits_a_string_into_several_substrings(string testString, int startIndex, int len, string[] expected)
+        {
+            var substrings = testString.Substrings(startIndex, len);
+
+            Assert.Equal(expected, substrings);
+        }
+    }
+}
---
+++ Samola.Numbers/Utilities/StringExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Samola.Numbers.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string[] Substrings(this string s, int startIndex, int length)
+        {
+            if (startIndex < 0)
+                throw new ArgumentException("startIndex must be non-negative.");
+
+            if (startIndex >= s.Length)
+                throw new IndexOutOfRangeException("startIndex must be within the string.");
+
+            int strLength = s.Length - startIndex;
+
+            var substrings = new List<string>(strLength / length + 1);
+
+            string t = s;
+            int tempStartIndex = startIndex;
+            bool isFirst = true;
+            while (t.Length > 0)
+            {
+                int len = Math.Min(length, t.Length - tempStartIndex);
+                var str = t.Substring(tempStartIndex, len);
+                t = t.Substring(tempStartIndex + len);
+
+                substrings.Add(str);
+                if (isFirst)
+                {
+                    tempStartIndex = 0;
+                    isFirst = false;
+                }
+            }
+
+            return substrings.ToArray();
+        }
+    }
+}
