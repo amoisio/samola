a8260c9e85c51c3b8df1cd3829a805f8a21c8e12
Aleksi Moisio <aleksi.moisio30@gmail.com>
2019-10-16 22:18:52 +0300

    Add ability to list fibonacci numbers

--- Samola.Numbers.Console/ShowFibonacci.cs
+++ Samola.Numbers.Console/ShowFibonacci.cs
@@ -19,21 +19,38 @@ namespace Samola.Numbers
             Console.Write("Maximum terms > ");
             int max = Int32.Parse(Console.ReadLine());
 
+            Console.Write("Display all (Y) > ");
+            bool displayAll = Console.ReadLine() == "Y";
+
             FibonacciNumbersBuilder builder = new FibonacciNumbersBuilder();
             builder.Limit = new LargeIntegerCountLimit(5000); // At most 5000 Fibonacci terms
             var numbers = builder.Build();
 
-            var terms = numbers.TakeWhile(n => n.ToString().Length <= max).ToArray();
+            if (displayAll)
+            {
+                int index = 1;
+                foreach(var number in numbers)
+                {
+                    Console.WriteLine($"F{index,-4}: {number}");
+                    if (number.ToString().Length == max)
+                        break;
+                    index++;
+                }
+            }
+            else
+            {
+                var terms = numbers.TakeWhile(n => n.ToString().Length <= max).ToArray();
 
-            var termsLessThan = terms.Where(n => n.ToString().Length < max).ToArray();
-            var len = termsLessThan.Length;
-            var a = termsLessThan[len - 2];
-            var b = termsLessThan[len - 1];
-            var d = a + b;
-            Console.WriteLine(termsLessThan[len - 2].ToString());
-            Console.WriteLine(termsLessThan[len - 1].ToString());
-            Console.WriteLine(terms.ToArray()[len].ToString());
-            Console.WriteLine(len);
+                var termsLessThan = terms.Where(n => n.ToString().Length < max).ToArray();
+                var len = termsLessThan.Length;
+                var a = termsLessThan[len - 2];
+                var b = termsLessThan[len - 1];
+                var d = a + b;
+                Console.WriteLine(termsLessThan[len - 2].ToString());
+                Console.WriteLine(termsLessThan[len - 1].ToString());
+                Console.WriteLine(terms.ToArray()[len].ToString());
+                Console.WriteLine(len + 1);
+            }
         }
     }
 }
