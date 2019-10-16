a847b1e09805af9bf3e4adb2149f758ea38a8934
Aleksi Moisio <aleksi.moisio30@gmail.com>
2019-10-16 21:56:06 +0300

    Change ShowFibonacci helper to show the term index

--- Samola.Numbers.Console/ShowFibonacci.cs
+++ Samola.Numbers.Console/ShowFibonacci.cs
@@ -20,36 +20,20 @@ namespace Samola.Numbers
             int max = Int32.Parse(Console.ReadLine());
 
             FibonacciNumbersBuilder builder = new FibonacciNumbersBuilder();
-            builder.Limit = new LargeIntegerCountLimit(5000);
-
+            builder.Limit = new LargeIntegerCountLimit(5000); // At most 5000 Fibonacci terms
             var numbers = builder.Build();
-            LargeInteger previous;
 
-            int c = max;
-            var arr = numbers.TakeWhile(n => n.ToString().Length <= c).ToArray();
+            var terms = numbers.TakeWhile(n => n.ToString().Length <= max).ToArray();
 
-            var arr1 = arr.Where(n => n.ToString().Length < c).ToArray();
-            var len = arr1.Length;
-            var a = arr1[len - 2];
-            var b = arr1[len - 1];
+            var termsLessThan = terms.Where(n => n.ToString().Length < max).ToArray();
+            var len = termsLessThan.Length;
+            var a = termsLessThan[len - 2];
+            var b = termsLessThan[len - 1];
             var d = a + b;
-            Console.WriteLine(arr1[len - 2].ToString());
-            Console.WriteLine(arr1[len - 1].ToString());
-            Console.WriteLine(arr.First(n => n.ToString().Length == c).ToString());
-
-
-            //foreach (var number in numbers)
-            //{
-            //    if (number.ToString().Length == 10)
-            //    {
-            //        Console.WriteLine($"{previous.ToString()}");
-            //        Console.WriteLine($"{number.ToString()}");
-            //        break;
-            //    }
-
-            //    previous = number;
-
-            //}
+            Console.WriteLine(termsLessThan[len - 2].ToString());
+            Console.WriteLine(termsLessThan[len - 1].ToString());
+            Console.WriteLine(terms.ToArray()[len].ToString());
+            Console.WriteLine(len);
         }
     }
 }
