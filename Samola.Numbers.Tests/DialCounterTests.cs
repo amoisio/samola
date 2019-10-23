b8c9d723a442addb6993225398cac782e012a5fc
Aleksi Moisio <aleksi.moisio30@gmail.com>
2019-10-23 23:28:56 +0300

    Add DialCounter

---
+++ Samola.Numbers.Tests/DialCounterTests.cs
@@ -0,0 +1,96 @@
+﻿using Samola.Numbers.Counters;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Samola.Numbers.Tests
+{
+    public class DialCounterTests
+    {
+        [Theory]
+        [MemberData(nameof(PrintData))]
+        public void DialCounter_can_print_itself_out(string expected, DialCounter counter)
+        {
+            Assert.Equal(expected, counter.ToString());
+        }
+
+        public static IEnumerable<object[]> PrintData =>
+            new List<object[]>
+            {
+                new object[] { "9999", new DialCounter(4) },
+                new object[] { "9746", new DialCounter(new int[] { 9, 7, 4, 6 }) }
+            };
+
+        [Theory]
+        [MemberData(nameof(Count1Data))]
+        public void DialCounter_counts_down_normally(string expected, DialCounter counter)
+        {
+            int dials = counter.Dials;
+
+            var ok = counter.Roll(dials - 1);
+
+            Assert.True(ok);
+            Assert.Equal(expected, counter.ToString());
+        }
+
+        public static IEnumerable<object[]> Count1Data =>
+            new List<object[]>
+            {
+                new object[] { "9998", new DialCounter(4) },
+                new object[] { "973", new DialCounter(new int[] { 9, 7, 4 }) }
+            };
+
+        [Fact]
+        public void DialCounter_can_we_initialized_with_number_of_dials()
+        {
+            DialCounter counter = new DialCounter(3);
+
+            Assert.Equal(3, counter.Dials);
+            Assert.Equal("999", counter.ToString());
+        }
+
+        [Fact]
+        public void DialCounter_can_we_initialized_with_dial_values()
+        {
+            DialCounter counter = new DialCounter(new int[] { 5, 4, 3, 2, 1 });
+
+            Assert.Equal(5, counter.Dials);
+            Assert.Equal("54321", counter.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(Count2Data))]
+        public void DialCounter_items_to_the_right_are_reset_on_count_down(string expected, DialCounter counter)
+        {
+            var ok = counter.Roll(1);
+
+            Assert.True(ok);
+            Assert.Equal(expected, counter.ToString());
+        }
+
+        public static IEnumerable<object[]> Count2Data =>
+            new List<object[]>
+            {
+                new object[] { "9888", new DialCounter(4) },
+                new object[] { "966", new DialCounter(new int[] { 9, 7, 4 }) }
+            };
+
+        [Theory]
+        [MemberData(nameof(Count3Data))]
+        public void DialCounter_items_are_reset_on_rollover(string expected, DialCounter counter)
+        {
+            var dials = counter.Dials;
+            var ok = counter.Roll(dials - 1);
+
+            Assert.Equal(expected, counter.ToString());
+        }
+
+        public static IEnumerable<object[]> Count3Data =>
+            new List<object[]>
+            {
+                new object[] { "8888", new DialCounter(new int[] { 9, 0, 0, 0 }) },
+                new object[] { "955", new DialCounter(new int[] { 9, 6, 0 }) }
+            };
+
+
+    }
+}
---
+++ Samola.Numbers/Counters/DialCounter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+
+namespace Samola.Numbers.Counters
+{
+    /// <summary>
+    /// DialCounter 
+    /// - Has N dials with numbers from 0 to 9
+    /// - Count-down dial - dials are initially at 9 and count down to 0
+    /// - TODO: Add count-up functionality
+    /// - Dials have different weights.
+    /// - The left most dial is most significant and the right most is least significant
+    /// - Dials rolls according to the following rules
+    /// - 1. Dials _never_ roll upwards
+    /// - 2. The exception to this is that dials can roll "roll over" from 0 back to 9
+    /// - 3. When a dial rolls normally, all dials to the left of it are unaffected
+    /// - 4. When a dial rolls normally, all dials to the right of it are reset to the same value 
+    /// -    i.e. 9743, 7 rolls to 6, new value is 9666
+    /// - 5. When a dial rolls over, the dial immediately to the left is rolled once
+    /// -    ie. 9703, 0 rolls to 9, intermediate value: 9693, new value is 9666
+    /// -    ie. 9000, last zero rolls to 9, changes: 9009, 9099, 9999, 8999, 8888
+    /// </summary>
+    public class DialCounter
+    {
+        private readonly int[] _dials;
+
+        public DialCounter(int n)
+        {
+            _dials = new int[n];
+            Dials = n;
+            InitializeDials();
+        }
+
+        public DialCounter(int[] dials)
+        {
+            _dials = dials;
+            Dials = dials.Length;
+        }
+
+        private void InitializeDials()
+        {
+            for (int i = 0; i < Dials; i++)
+            {
+                _dials[i] = 9;
+            }
+        }
+
+        public int Dials { get; }
+
+        /// <summary>
+        /// Roll dial once
+        /// </summary>
+        /// <param name="dialIndex">0 based index of the dial. 0 being the most significant dial.</param>
+        public bool Roll(int dialIndex)
+        {
+            if (dialIndex < 0 || dialIndex >= Dials)
+                throw new IndexOutOfRangeException("dialIndex is out of range");
+
+            int oldValue = _dials[dialIndex];
+            bool isRollOver = (oldValue == 0);
+
+            int affectedIndex = -1;
+            if (isRollOver)
+            {
+                for (int i = dialIndex - 1; i >= 0; i--)
+                {
+                    if (_dials[i] > 0)
+                    {
+                        affectedIndex = i;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                affectedIndex = dialIndex;
+            }
+
+            if (affectedIndex == -1)
+            {
+                // The counter has been spent.
+                return false;
+            }
+            else
+            {
+                int newValue = _dials[affectedIndex] - 1;
+                for (int i = affectedIndex; i < Dials; i++)
+                {
+                    _dials[i] = newValue;
+                }
+                return true;
+            }
+        }
+
+        public void Reset()
+        {
+            InitializeDials();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Dials; i++)
+            {
+                sb.Append(_dials[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
