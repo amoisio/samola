a816ea702e527f61aff875b335a7f3d7311b3a82
Aleksi Moisio <aleksi.moisio30@gmail.com>
2019-11-25 18:16:21 +0200

    Add Samola.Linq.Extensions

---
+++ Samola.Linq.Extensions/Samola.Linq.Extensions/Samola.Linq.Extensions.Tests/TestEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Samola.Linq.Extensions.Tests
+{
+    public class TestEntity
+    {
+        public int Key { get; set; }
+        public string Opt { get; set; }
+        public string Value => $"{typeof(TestEntity).Name}_{Key}"; 
+    }
+}
